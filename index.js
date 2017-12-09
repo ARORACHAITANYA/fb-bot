1334ec8677546feacaf56cf463a8321fba7a3ba6
Chaitanya Arora <arorachaitanya1@gmail.com>
2017-12-09 20:56:39 +0530

    corrected webhook_event variable

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/webhook', (req, res) => {
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
-      let webhookEvent = entry.messaging[0];
+      let webhook_event = entry.messaging[0];
       console.log(webhookEvent);
 
       // Get the sender PSID
